a3bf9985fc9dc4a76ed9365e4fb20b6e7595f004
hamza lamjounah <lamjounah.prf@gmail.com>
2026-05-13 13:08:49 +0200

    first commit

---
+++ src/main/java/ma/atos/billing/invoice/billing_invoice/BillingInvoiceApplication.java
@@ -0,0 +1,13 @@
+package ma.atos.billing.invoice.billing_invoice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillingInvoiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillingInvoiceApplication.class, args);
+	}
+
+}
